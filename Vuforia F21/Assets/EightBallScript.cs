e234b7e6dc4ddc0bc561ef3a40e5a9cb5b52d113
ddanciu2 <ddanci2@uic.edu>
2021-09-18 15:09:56 -0500

    Few updates, C# script and apk file

--- Vuforia F21/Assets/EightBallScript.cs
+++ Vuforia F21/Assets/EightBallScript.cs
@@ -5,57 +5,48 @@ using TMPro;
 
 public class EightBallScript : MonoBehaviour
 {
-
+    //Pick a random number to choose 8ball saying
     private int PickRandomNumber(){
         int randomNum = Random.Range(1,4);
         return randomNum;
     }
 
-    public GameObject Magic8Text;
-    public Transform CubePositionDetection;
+    public GameObject Magic8Text; //Text to change
+    public Transform CubePositionDetection; //Cube base itself to detect position
+    public AudioSource TextChangeSE; //Audio to play when Cube changes text
 
-    public AudioSource TextChangeSE;
+    bool upsideDownFlag = false; //Flag to signal that cube was flipped
+    int randomNum; //Assign random # to choose between pos, neutral, neg
+    int randomSayingIndex; //Choose random index in array of sayings
+    string assignText; //String to store assigned text
 
-    bool upsideDownFlag = false;
-    int randomNum;
-    int randomSayingIndex;
-    string assignText;
-
+    //List of sayings (custom)
     string[] eightBallPositive = new string[5] {"100% Yes", "Yesss", "Let's Go! Yes!", "No Doubt, answer = yes", "Looking like a yes"};
     
     string[] eightBallNeutral = new string[3] {"I don't know boss", "No idea", "Repeat the question"};
     string[] eightBallNegative = new string[4] {"Fat L, answer = no", "Bruh, no way", "No Shot","Sorry fam, but no"};
 
-    // Start is called before the first frame update
     void Start()
     {
-       InvokeRepeating("UpdateEightBall", 2f, 0.5f);
+       InvokeRepeating("UpdateEightBall", 2f, 0.5f); //Call UpdateEightBall
     }
 
 
     void UpdateEightBall()
     {
-        float x = CubePositionDetection.eulerAngles.x;
-        float y = CubePositionDetection.eulerAngles.y;
+        //Get z value of base cube
         float z = CubePositionDetection.eulerAngles.z;
         
-        // Debug.Log("z: " + z);
-
-
-        // if(y > 140 && y < 220 || z > 130 && z < 220){
+        //Cube is upside down
         if(z > 130 && z < 220){
             randomNum = PickRandomNumber();
             upsideDownFlag = true;
-            Debug.Log("AZ Flipped");
         }
 
-        // if(y < 80 || y > 330 || z > 330 || z < 30){
+        //Cube is rightside up
         if(z > 330 || z < 30){
-            Debug.Log("AZ Upright");
-
+            //Check if cube was flipped first, then change text/play audio
             if(upsideDownFlag == true){
-                Debug.Log("AZ CHANGE TEXT");
-
                 upsideDownFlag = false;
                 if(randomNum == 1){
                     int randomSayingIndex = Random.Range(0,eightBallPositive.Length-1);
@@ -68,12 +59,9 @@ public class EightBallScript : MonoBehaviour
                     int randomSayingIndex = Random.Range(0,eightBallNegative.Length-1);
                     assignText = eightBallNegative[randomSayingIndex];
                 }
-
-                Debug.Log("New Text: " + assignText);
                 TextChangeSE.Play();
                 Magic8Text.GetComponent<TextMeshPro>().text = assignText;
             }
         }
-
     }
 }
--- Vuforia F21/Assets/EightBallScriptOriginal.cs
+++ Vuforia F21/Assets/EightBallScriptOriginal.cs
@@ -5,56 +5,48 @@ using TMPro;
 
 public class EightBallScriptOriginal : MonoBehaviour
 {
-
+    //Pick a random number to choose 8ball saying
     private int PickRandomNumber(){
         int randomNum = Random.Range(1,4);
         return randomNum;
     }
 
-    public GameObject Magic8Text;
-    public Transform CubePositionDetection;
+    public GameObject Magic8Text; //Text to change
+    public Transform CubePositionDetection; //Cube base itself to detect position
+    public AudioSource TextChangeSE; //Audio to play when Cube changes text
 
-    public AudioSource TextChangeSE;
+    bool upsideDownFlag = false; //Flag to signal that cube was flipped
+    int randomNum; //Assign random # to choose between pos, neutral, neg
+    int randomSayingIndex; //Choose random index in array of sayings
+    string assignText; //String to store assigned text
 
-    bool upsideDownFlag = false;
-    int randomNum;
-    int randomSayingIndex;
-    string assignText;
-
+    //List of sayings (original)
     string[] eightBallPositive = new string[10] {"It is Certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes.", "Most likely", "Outlook good", "Yes", "Signs point to yes"};
 
     string[] eightBallNeutral = new string[5] {"Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"};
     string[] eightBallNegative = new string[5] {"Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
 
-    // Start is called before the first frame update
     void Start()
     {
-       InvokeRepeating("UpdateEightBall", 2f, 0.5f);
+       InvokeRepeating("UpdateEightBall", 2f, 0.5f); //Call UpdateEightBall
     }
 
 
     void UpdateEightBall()
     {
-        float x = CubePositionDetection.eulerAngles.x;
-        float y = CubePositionDetection.eulerAngles.y;
+        //Get z value of base cube
         float z = CubePositionDetection.eulerAngles.z;
-        
 
-        // Debug.Log("y " + y);
-
-
+        //Cube is upside down
         if(z > 130 && z < 220){
             randomNum = PickRandomNumber();
             upsideDownFlag = true;
-            Debug.Log("Japan Flipped");
         }
 
+        //Cube is rightside up
         if(z > 330 || z < 30){
-            Debug.Log("Japan Upright");
-
-            if(upsideDownFlag == true){
-                Debug.Log("JAPAN CHANGE TEXT");
-                
+            //Check if cube was flipped first, then change text/play audio
+            if(upsideDownFlag == true){             
                 upsideDownFlag = false;
                 if(randomNum == 1){
                     int randomSayingIndex = Random.Range(0,eightBallPositive.Length-1);
@@ -67,13 +59,9 @@ public class EightBallScriptOriginal : MonoBehaviour
                     int randomSayingIndex = Random.Range(0,eightBallNegative.Length-1);
                     assignText = eightBallNegative[randomSayingIndex];
                 }
-                Debug.Log("New Text: " + assignText);
                 TextChangeSE.Play();
                 Magic8Text.GetComponent<TextMeshPro>().text = assignText;
             }
         }
-
     }
 }
-
-
